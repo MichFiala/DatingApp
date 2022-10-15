d433708dffc5fe1ee553de265b70b59dfad33949
Michal Fiala <michfiala.it@gmail.com>
2022-10-15 13:50:16 +0200

    Initial

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<AppUser> Users { get; set; }
+	public DataContext(DbContextOptions options) : base(options)
+	{
+	}
+}
